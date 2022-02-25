c7db898d1cb8ec9113b5b6189d001c87602b8f96
Jorge Trad <jetradl@gmail.com>
2022-02-25 08:21:49 -0600

    Initial state with next and react

---
+++ Components/EncryptingForm/index.jsx
@@ -0,0 +1,5 @@
+const EncryptingForm = () => {
+    return <h1>Hola</h1>
+}
+
+export default EncryptingForm;
---
+++ pages/encryption-techniques.jsx
@@ -0,0 +1,5 @@
+const EncryptionTechniques = () => {
+    return <h1>Hola</h1>
+}
+
+export default EncryptionTechniques;
